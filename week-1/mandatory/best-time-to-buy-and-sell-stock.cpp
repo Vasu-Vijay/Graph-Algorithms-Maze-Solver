5cad94ff4e74686ebaf0c663be9b500e33f29094
Vasu-Vijay <palasusetu@gmail.com>
2026-05-25 20:06:57 +0000

    add best-time-to-buy-and-sell-stock.cpp

---
+++ week-1/mandatory/best-time-to-buy-and-sell-stock.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int ans=0, min=prices[0];
+        for(int i=0; i<prices.size(); i++) {
+            if(prices[i]<min) {
+                min=prices[i];
+            }
+            ans=max(ans, prices[i]-min);
+        }
+        return ans;
+    }
+};
