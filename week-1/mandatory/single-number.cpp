5c1c04e8b64a4dc1118b8f0d016a03d6c61088ba
Vasu-Vijay <palasusetu@gmail.com>
2026-05-28 07:50:58 +0000

    improve single-number.cpp by using XOR

--- week-1/mandatory/single-number.cpp
+++ week-1/mandatory/single-number.cpp
@@ -1,16 +1,11 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        unordered_set<int> done;
+
         int n = nums.size();
         int ans=0;
         for(int i=0; i<n; i++) {
-            if(done.count(nums[i])) {
-                ans-=nums[i];
-            } else {
-                ans+=nums[i];
-                done.insert(nums[i]);
-            }
+            ans^=nums[i];
         }
         return ans;
     }    
