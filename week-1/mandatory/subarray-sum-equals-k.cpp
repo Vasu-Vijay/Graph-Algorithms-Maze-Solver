3036b8ae016be8fb7c77b4db85a89e2d40736636
Vasu-Vijay <palasusetu@gmail.com>
2026-05-29 20:32:03 +0000

    improve subarray-sum-equals-k.cpp by using a better map structure

--- week-1/mandatory/subarray-sum-equals-k.cpp
+++ week-1/mandatory/subarray-sum-equals-k.cpp
@@ -3,22 +3,17 @@ public:
     int subarraySum(vector<int>& nums, int k) {
         int n = nums.size(), reqnum, ans=0;
         vector<int> cumsum(n, 0); 
-        unordered_multimap<int, int> map;
+        unordered_map<int, int> map;
         cumsum[0]=nums[0];
         if(cumsum[0]==k) { ans++; }
-        map.insert({cumsum[0], 0});
+        map.insert({cumsum[0], 1});
         for(int i=1; i<n; i++) {
             cumsum[i]=cumsum[i-1]+nums[i];
             if(cumsum[i]==k) ans++;
-            map.insert({cumsum[i], i});
-            reqnum=cumsum[i]-k;
-            auto p=map.equal_range(reqnum);
-            for(auto reqi=p.first; reqi!=p.second; reqi++) {
-                if(reqi!=map.end() && reqi->second != i) {
-                    ans++;
-                }
-            }
             
+            reqnum=cumsum[i]-k;
+            ans+=map[reqnum];
+            map[cumsum[i]]+=1;
         }
         return ans;
     }
