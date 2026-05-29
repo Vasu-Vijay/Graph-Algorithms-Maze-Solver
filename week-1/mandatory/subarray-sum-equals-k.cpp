adb78996382e5211fa268e58f31e153bc25a191c
Vasu-Vijay <palasusetu@gmail.com>
2026-05-29 20:35:29 +0000

    clean code in subarray-sum-equals-k.cpp

--- week-1/mandatory/subarray-sum-equals-k.cpp
+++ week-1/mandatory/subarray-sum-equals-k.cpp
@@ -1,19 +1,15 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-        int n = nums.size(), reqnum, ans=0;
-        vector<int> cumsum(n, 0); 
+        int n = nums.size(), reqnum, ans=0, cumsum=0;
         unordered_map<int, int> map;
-        cumsum[0]=nums[0];
-        if(cumsum[0]==k) { ans++; }
-        map.insert({cumsum[0], 1});
-        for(int i=1; i<n; i++) {
-            cumsum[i]=cumsum[i-1]+nums[i];
-            if(cumsum[i]==k) ans++;
-            
-            reqnum=cumsum[i]-k;
+        for(int i=0; i<n; i++) {
+            cumsum+=nums[i];
+            if(cumsum==k) ans++;
+
+            reqnum=cumsum-k;
             ans+=map[reqnum];
-            map[cumsum[i]]+=1;
+            map[cumsum]+=1;
         }
         return ans;
     }
