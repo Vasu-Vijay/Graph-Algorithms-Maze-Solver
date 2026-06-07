c55b27ef7ae4454498a334fc8a21a5b4b5fc852c
Vasu-Vijay <palasusetu@gmail.com>
2026-06-07 17:47:31 +0000

    s: solve top-k-frequent-elements.cpp

--- week-2/mandatory/top-k-frequent-elements.cpp
+++ week-2/mandatory/top-k-frequent-elements.cpp
@@ -1,76 +1,22 @@
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        sort(nums.begin(), nums.end());
-        int prev = nums[0], n = nums.size(), count = 1, maxcount = 1;
-        if (n == 1) {
-            return {nums[0]};
+        int n=nums.size();
+        unordered_map<int, int> freqs;
+        for(int i=0; i<n; i++) {
+            freqs[nums[i]]++;
         }
-        queue<int> q;
-        bool qmade = false;
-        vector<pair<int, int>> tempq;
-        for (int i = 1; i < n; i++) {
-            if (nums[i] == prev) {
-                count++;
-            } else {
-                if (qmade) {
-                    if (count >= maxcount) {
-                        maxcount = count;
-                        q.push(prev);
-                        if (q.size() > k) {
-                            q.pop();
-                        }
-                    }
-                } else {
-                    tempq.push_back({prev, count});
-                    if (tempq.size() == k) {
-                        qmade = true;
-                        sort(tempq.begin(), tempq.end(),
-                             [](const auto& a, const auto& b) {
-                                 return a.second < b.second;
-                             });
-                        maxcount = tempq[k-1].second;
-                        for (int j = 0; j < k; j++) {
-                            q.push(tempq[j].first);
-                        }
-                    }
-                }
-                prev = nums[i];
-                count = 1;
-            }
+        vector<pair<int,int>> map;
+        for(auto it=freqs.begin(); it!=freqs.end(); it++) {
+            map.push_back({it->second, it->first});
         }
-        if (qmade) {
-            if (count >= maxcount) {
-                maxcount = count;
-                q.push(prev);
-                if (q.size() > k) {
-                    q.pop();
-                }
-            }
-        } else {
-            tempq.push_back({prev, count});
-            if (tempq.size() == k) {
-                qmade = true;
-                sort(tempq.begin(), tempq.end(),
-                     [](const auto& a, const auto& b) {
-                         return a.second < b.second;
-                     });
-                for (int j = 0; j < k; j++) {
-                    q.push(tempq[j].first);
-                }
-            }
-        }
+        sort(map.begin(), map.end(), [](const auto& a, const auto& b) {
+            return a.first > b.first;
+        });
         vector<int> ans(k);
-        for (int i = 0; i < k; i++) {
-            ans[i] = q.front();
-            q.pop();
+        for(int i=0; i<k; i++) {
+            ans[i]=map[i].second;
         }
         return ans;
     }
 };
-
-// -2, -1, -1, 0, 1, 2, 3, 4, 4, 4
-
-// -9, -3, -1, -1, 1, 2, 2, 3, 3, 3, 3, 5, 7, 7, 7, 9, 9, 10, 10, 10
-
-//-9 -3 -1 2 3
