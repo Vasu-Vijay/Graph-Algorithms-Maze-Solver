8cc1a66857184afc22cb998c5cbfe8e48218a913
Vasu-Vijay <palasusetu@gmail.com>
2026-06-12 10:58:13 +0000

    s: solve combination-sum.cpp

--- week-2/mandatory/combination-sum.cpp
+++ week-2/mandatory/combination-sum.cpp
@@ -1,67 +1,26 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-
 class Solution {
 public:
-
-    void getAns(unordered_map<int, vector<vector<int>>>& answers,
-                vector<vector<int>> curanswers, vector<int> cursum,
-                vector<int>& candidates, int target, int ogtarget) {
-        if (answers.count(target)) {
-            for (int i = 0; i < answers[target].size(); i++) {
-                if(!answers.count(ogtarget)) {
-                    answers[ogtarget]={{}};
-                }
-                for (int j = 0; j < answers[target][i].size(); j++) {
-                    answers[ogtarget].back().push_back(answers[target][i][j]);
-                }
-                for (int j = 0; j < cursum.size(); j++) {
-                    answers[ogtarget].back().push_back(cursum[j]);
-                }
-
-            }
-        } else {
-            if (target == 0) {
-                curanswers.push_back({});
-                for(int i=0; i< cursum.size(); i++) {
-                    curanswers.back().push_back(cursum[i]);
-                }
-                cursum.clear();
-                return;
-            } else {
-                for (int i = 0; i < candidates.size(); i++) {
-                    if(target<candidates[i]) {
-                        continue;
-                    }
-                    cursum.push_back(candidates[i]);
-                    getAns(answers, curanswers, cursum, candidates, target-candidates[i], target);
-                }
-                answers.insert({target, curanswers});
-                curanswers.clear();
-            }
+    void backTrackAddNum(int start_idx, int sum, int target, vector<int>& ans, vector<vector<int>>& answers, vector<int> &candidates) {
+        if(sum == target) {
+            answers.push_back(ans);
+            return;
         }
+        for(int i=start_idx; i<candidates.size(); i++) {
+            if(candidates[i] > target - sum) {
+                continue;
+            }
+            ans.push_back(candidates[i]);
+            backTrackAddNum(i, sum + candidates[i], target, ans, answers, candidates);
+            ans.pop_back();
+        }
+        return;
     }
 
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        unordered_map<int, vector<vector<int>>> answers;
-        vector<vector<int>> curanswers;
-        vector<int> cursum;
-        getAns(answers, curanswers, cursum, candidates, target, target);
-        return answers[target];
+        vector<vector<int>> answers;
+        vector<int> ans;
+        sort(candidates.begin(), candidates.end());
+        backTrackAddNum(0, 0, target, ans, answers, candidates);
+        return answers;
     }
 };
-
-int main() {
-    vector<int> candidates = {2,3,6,7};
-    int target = 7;
-    Solution soln;
-    vector<vector<int>> ans = soln.combinationSum(candidates, target);
-    for(auto &x: ans) {
-        cout<<"[";
-        for(auto &y: x) {
-            cout<<y<<",";
-        }
-        cout<<"],";
-    } 
-}
