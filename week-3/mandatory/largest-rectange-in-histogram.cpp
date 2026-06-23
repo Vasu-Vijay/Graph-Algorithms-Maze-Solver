4c5cf9cf0b58d98395138fd2fbeb4958849fbbbb
Vasu-Vijay <palasusetu@gmail.com>
2026-06-23 12:11:47 +0000

    h: solve largest-rectangle-in-histogram.cpp

--- week-3/mandatory/largest-rectange-in-histogram.cpp
+++ week-3/mandatory/largest-rectange-in-histogram.cpp
@@ -2,20 +2,29 @@ class Solution {
 public:
     int largestRectangleArea(vector<int>& heights) {
         int n = heights.size();
-        int maxm=0;
-        stack<int> st;
-        for(int i=0; i<n; i++) {
-            while(!st.empty() && heights[st.top()]>heights[i]) {
-                maxm = max(maxm, (i - st.top()) * heights[st.top()]);
-                st.pop();
+        int maxm = 0;
+        stack<int> stL, stR;
+        vector<int> left(n, -1), right(n, n);
+        for (int i = 0; i < n; i++) {
+            while (!stR.empty() && heights[stR.top()] > heights[i]) {
+                right[stR.top()] = i;
+                stR.pop();
             }
-            st.push(i);
+            stR.push(i);
         }
-        int first = n;
-        while(!st.empty()) {
-            first = st.top();
-            st.pop();
-        } 
-        return max(maxm, (n-first) * heights[first]);
+
+        for(int i=n-1; i>=0; i--) {
+            while (!stL.empty() && heights[stL.top()] > heights[i]) {
+                left[stL.top()] = i;
+                stL.pop();
+            }
+            stL.push(i);
+        }
+        
+        for(int i=0; i<n; i++) {
+            maxm = max(maxm, (right[i] - left[i] - 1) * heights[i]);
+        }
+
+        return maxm;
     }
 };
